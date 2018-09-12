5fa0d4d1a4b9a16311baebb4c65a2ad32f0da485
olli <oltsuwh@gmail.com>
2018-09-12 16:09:44 +0300

    fixed linting errors

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BaseQuestion' }]
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'BaseQuestion' }]
 })
 
 const User = mongoose.model('User', userSchema)
