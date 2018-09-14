66f6eb8665274dadac98e58f4c151f9a19b55fc0
woltsu <oltsuwh@gmail.com>
2018-09-14 16:59:19 +0300

    changed the logic of questions and their structure. added api for answering questions

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -2,10 +2,11 @@ const questionRouter = require('express').Router()
 const BaseQuestion = require('../models/baseQuestion')
 const PrintQuestion = require('../models/printQuestion')
 const CompileQuestion = require('../models/compileQuestion')
+const CorrectAnswer = require('../models/correctAnswer')
 
 questionRouter.get('/', async (req, res) => {
   try {
-    const baseQuestions = await BaseQuestion.find().populate('question.item')
+    const baseQuestions = await BaseQuestion.find().populate('question.item').populate('correctAnswer')
     res.status(200).json(baseQuestions)
   } catch (e) {
     console.error('e', e)
@@ -16,8 +17,8 @@ questionRouter.get('/', async (req, res) => {
 questionRouter.get('/random', async (req, res) => {
   try {
     const baseQuestions = await BaseQuestion.find().populate('question.item')
-    const question = baseQuestions[Math.floor(Math.random() * (baseQuestions.length))]
-    res.status(200).json(BaseQuestion.format(question))
+    const baseQuestion = baseQuestions[Math.floor(Math.random() * (baseQuestions.length))]
+    res.status(200).json(baseQuestion.question)
   } catch (e) {
     console.error('e', e)
     res.status(500)
@@ -26,12 +27,15 @@ questionRouter.get('/random', async (req, res) => {
 
 questionRouter.post('/print', async (req, res) => {
   try {
-    const { value, correctAnswer, wrongAnswers } = req.body
-    const newPrintQuestion = new PrintQuestion({ value, correctAnswer, wrongAnswers })
+    const { value, correctAnswer, options } = req.body
+    const newCorrectAnswer = new CorrectAnswer({ value: correctAnswer })
+    await newCorrectAnswer.save()
+    const newPrintQuestion = new PrintQuestion({ value, options: options.concat(correctAnswer) })
     await newPrintQuestion.save()
     const newBaseQuestion = new BaseQuestion({
       type: 'print',
-      question: { kind: 'PrintQuestion', item: newPrintQuestion._id }
+      question: { kind: 'PrintQuestion', item: newPrintQuestion._id },
+      correctAnswer: newCorrectAnswer._id
     })
     const result = await newBaseQuestion.save()
     res.status(201).json(result)
@@ -43,18 +47,33 @@ questionRouter.post('/print', async (req, res) => {
 
 questionRouter.post('/compile', async (req, res) => {
   try {
-    const { correctAnswer, wrongAnswers } = req.body
-    const newCompileQuestion = new CompileQuestion({ correctAnswer, wrongAnswers })
+    const { correctAnswer, options } = req.body
+    const newCorrectAnswer = new CorrectAnswer({ value: correctAnswer })
+    await newCorrectAnswer.save()
+    const newCompileQuestion = new CompileQuestion({ options: options.concat(correctAnswer) })
     await newCompileQuestion.save()
     const newBaseQuestion = new BaseQuestion({
       type: 'compile',
-      question: { kind: 'CompileQuestion', item: newCompileQuestion._id }
+      question: { kind: 'CompileQuestion', item: newCompileQuestion._id },
+      correctAnswer: newCorrectAnswer._id
     })
     const result = await newBaseQuestion.save()
     res.status(201).json(result)
   } catch (e) {
     console.error('e', e)
     res.status(500)
+  }
+})
+
+questionRouter.post('/answer', async (req, res) => {
+  try {
+    const { id, answer } = req.body
+    const answeredQuestion = await BaseQuestion.findOne({ 'question.item': id }).populate('correctAnswer')
+    const isCorrectAnswer = answer === answeredQuestion.correctAnswer.value
+    res.status(200).json({ isCorrect: isCorrectAnswer })
+  } catch (e) {
+    console.error('e', e)
+    res.status(500).json({ error: 'error' })
   }
 })
 
