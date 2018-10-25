be13334ecc6e3f2eac486950c38f8819af16a141
woltsu <oltsuwh@gmail.com>
2018-10-26 01:20:42 +0300

    Added time attribute to Answer schema and storing of time

--- src/models/answer.js
+++ src/models/answer.js
@@ -4,7 +4,8 @@ const answerSchema = new mongoose.Schema({
   question: { type: mongoose.Schema.Types.ObjectId, ref: 'BaseQuestion' },
   user: { type: mongoose.Schema.Types.Number, ref: 'User' },
   isCorrect: mongoose.Schema.Types.Boolean,
-  timestamp : { type : mongoose.Schema.Types.Date, default: Date.now }
+  timestamp : { type : mongoose.Schema.Types.Date, default: Date.now },
+  time: mongoose.Schema.Types.Number
 })
 
 const Answer = mongoose.model('Answer', answerSchema)
