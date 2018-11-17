79d370d88966e7c31a6c866cfd35a31eb887491b
woltsu <oltsuwh@gmail.com>
2018-11-17 13:59:29 +0200

    added tests for courseController

---
+++ tests/controllers/courseController.test.js
@@ -0,0 +1,66 @@
+const supertest = require('supertest')
+const { app, server, apiUrl } = require('../../src/server')
+const api = supertest(app)
+const Course = require('../../src/models/course')
+const testUrl = `${apiUrl}/courses`
+
+describe('course controller', () => {
+  beforeEach(async () => {
+    // Remove all DB entities
+    await Course.deleteMany()
+
+    // Create some test courses
+    const course1 = new Course({ name: 'test1' })
+    await course1.save()
+    const course2 = new Course({ name: 'test2' })
+    await course2.save()
+  })
+
+  describe(`${apiUrl}/`, () => {
+    test('GET', async () => {
+      // Check that all courses are returned
+      const response = await api
+        .get(testUrl)
+      expect(response.status).toBe(200)
+      expect(response.body.length).toBe(2)
+    })
+
+    test('POST', async () => {
+      // Check that a new course can be created
+      let response = await api
+        .post(testUrl)
+        .send({ name: 'new' })
+      expect(response.status).toBe(201)
+      expect(response.body.name).toBe('new')
+      const foundCourse = await Course.find({ name: 'new' })
+      expect(foundCourse).toBeDefined()
+
+      // Check validation
+      response = await api
+        .post(testUrl)
+        .send({})
+      expect(response.status).toBe(422)
+      expect(response.body.error).toBe('course name missing')
+    })
+  })
+
+  describe(`${apiUrl}/:name`, async () => {
+    test('GET', async () => {
+      // Check that a course can be found by its name
+      let response = await api
+        .get(`${testUrl}/test1`)
+      expect(response.status).toBe(200)
+      expect(response.body.name).toBe('test1')
+
+      // Check that a course which doesn't exist won't be returned
+      response = await api
+        .get(`${testUrl}/none`)
+      expect(response.status).toBe(404)
+      expect(response.body.error).toBe('course not found')
+    })
+  })
+})
+
+afterAll(() => {
+  server.close()
+})
